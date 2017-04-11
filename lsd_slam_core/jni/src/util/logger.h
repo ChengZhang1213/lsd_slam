6541a6b4a7c3d91287fed15b6424009d16e9350f
aarontang <striversist@qq.com>
2017-04-11 11:26:35 +0800

    [log]Use new logger, __FILE__ is so long causing overflow, use __FUNCTION__ and __LINE__ while keeping LSDNative tag

--- lsd_slam_core/jni/src/util/logger.h
+++ lsd_slam_core/jni/src/util/logger.h
@@ -7,6 +7,7 @@
 
 
 #define STRINGIFY(x) #x
+#if 0
 #define LOG_TAG    __FILE__ ":" STRINGIFY(LSDNative)
 #ifdef LOGD
 #undef LOGD
@@ -17,6 +18,26 @@
 #undef LOGE
 #endif
 #define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#endif
+
+#define LOG_TAG     STRINGIFY(LSDNative)
+#ifdef LOGD
+#undef LOGD
+#endif
+#define LOGD(x...) do { \
+    char buf[512]; \
+    sprintf(buf, x); \
+    __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "%s | %i: %s", __FUNCTION__, __LINE__, buf); \
+} while (0)
+
+#ifdef LOGE
+#undef LOGE
+#endif
+#define LOGE(x...) do { \
+    char buf[512]; \
+    sprintf(buf, x); \
+    __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "%s | %i: %s", __FUNCTION__, __LINE__, buf); \
+} while (0)
 
 void printTrans(const Sophus::Sim3f::Transformation& trans);
 void printMatrix4f(const Sophus::Matrix4f& m);
